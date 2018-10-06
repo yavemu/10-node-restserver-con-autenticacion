276a6b2239d490ce1f5f0fdd743e1713f14043b2
YAM v.m <yavemu@gmail.com>
2018-10-06 14:03:24 -0500

    Creando variable de configuracion MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://usermlab:passmlab123@ds125618.mlab.com:25618/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
