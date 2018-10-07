4e5a70cabfa345a0659fdeaea81ba23b5d53251d
YAM v.m <yavemu@gmail.com>
2018-10-06 22:00:35 -0500

    Agregando la opcion de login con sus respectivas validaciones

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,8 +7,47 @@ const app = express();
 
 app.post('/login', (req, res) => {
 
-    res.json({
-        ok: true
+    let body = req.body;
+
+    // validamos que existan los campos requeridos desde el POST
+    if (!body.password || !body.email) {
+        return res.status(412).json({
+            ok: false,
+            err: {
+                message: "Parametros incompletos."
+            }
+        });
+    }
+
+    let condicion = { email: body.email };
+
+    Usuario.findOne(condicion, (err, usuarioDB) => {
+
+        // Este error se ejecuta cuando es generado por el servidor (500)
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        // Se valida si el email no existe (!usuarioDB => retornaria null) 
+        // o si la contraseña no coincide (Se usa bcrypt.compareSync)
+        if (!usuarioDB || !bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Usuario o Contraseña son incorrectos."
+                }
+            });
+        }
+
+        return res.json({
+            ok: true,
+            usuarioDB,
+            token: '123'
+        });
+
     });
 
 });
